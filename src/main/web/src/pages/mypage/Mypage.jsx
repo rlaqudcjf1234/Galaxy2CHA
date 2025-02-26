95238c6e430135267d5787b5b2263810df89710a
LEEJAEUK\jeauk <jeauk03@gmail.com>
2025-02-26 14:50:49 +0900

    변경사항 반영

--- src/main/web/src/pages/mypage/Mypage.jsx
+++ src/main/web/src/pages/mypage/Mypage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { authenticatedRequest as axios } from '../../plugins/axios';
 import { useParams } from 'react-router-dom';
 import '../../css/Mypage.css';
 import AddEduModal from './AddEduModal';
